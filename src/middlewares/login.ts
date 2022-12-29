14384ea84eabafcb28e4d123d6a06c0a1dfc9bc1
David Avalos <davalos@itparral.edu.mx>
2022-12-29 00:54:28 -0600

    fix: transporta las datos del usuario despues de validarlos

--- src/middlewares/login.ts
+++ src/middlewares/login.ts
@@ -1,12 +1,16 @@
 import type { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
 
+import { UsuarioLogeado, RequestConUsuario } from '../types'
+
 export const validarToken = (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(' ')[1]
     if (token) {
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET as string) 
-            // req.
+            const info: UsuarioLogeado = jwt.decode(token) as UsuarioLogeado
+
+            req.usuario = info
             next()
         } catch (error) {
             return res.status(401).json({ message: 'Error al validar el token', error })
